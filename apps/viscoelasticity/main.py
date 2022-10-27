64b6d008eb5881ae5f0d1c900caee81e39e480fb
Herminio <H.TasinafoHonorio@tudelft.nl>
2022-10-27 18:41:33 +0200

    Added viscoelastic model

---
+++ apps/viscoelasticity/main.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import json
+import os
+import sys
+sys.path.append(os.path.join("..", "..", "libs"))
+from AnalyticSolutions import *
+
+def read_json(file_name):
+	with open(file_name, "r") as j_file:
+		data = json.load(j_file)
+	return data
+
+def save_json(data, file_name):
+	with open(file_name, "w") as f:
+	    json.dump(data, f, indent=4)
+
+def write_stresses():
+	j_file = "settings.json"
+
+	# Read settings
+	settings = read_json(j_file)
+
+	# Define number of time steps
+	n_steps = 10
+
+	# Define time levels
+	settings["time"] = list(np.linspace(0, 2*day, n_steps))
+
+	# Define stress tensors
+	settings["sigma_zz"] = list(np.repeat(15*MPa, n_steps))
+	settings["sigma_xx"] = list(np.repeat(5*MPa, n_steps))
+	settings["sigma_yy"] = list(np.repeat(5*MPa, n_steps))
+	settings["sigma_xy"] = list(np.repeat(0.0, n_steps))
+	settings["sigma_yz"] = list(np.repeat(0.0, n_steps))
+	settings["sigma_xz"] = list(np.repeat(0.0, n_steps))
+
+	# Dump to file
+	save_json(settings, "settings.json")
+
+def main():
+	# Deine output folder
+	output_folder = os.path.join("output", "test")
+
+	# Write stresses, if necessary
+	# write_stresses()
+
+	# Read settings
+	settings = read_json("settings.json")
+
+	# Initialize viscoelastic model
+	model_ve = ViscoElastic(settings)
+
+	# Compute viscoelastic strains
+	model_ve.compute_strains()
+
+	# Save results
+	saver_eps_ve = TensorSaver(output_folder, "eps_ve")
+	saver_eps_ve.save_results(model_ve.time_list, model_ve.eps_ve)
+
+	# Save settings
+	save_json(settings, os.path.join(output_folder, "settings.json"))
+
+
+
+
+if __name__ == '__main__':
+	main()
--- libs/AnalyticSolutions.py
+++ libs/AnalyticSolutions.py
@@ -31,39 +31,94 @@ def voigt2tensor(s):
 			[s[4], s[5], s[2]],
 		])
 
-def build_stress_increments(sigmas):
-	d_sigma_list = []
-	for i in range(1, len(sigmas)):
-		d_sigma_list.append(sigmas[i] - sigmas[i-1])
-	return d_sigma_list
-
 class TensorSaver():
-	def __init__(self, name):
-		self.name = name
+	def __init__(self, output_folder, file_name):
+		self.file_name = file_name
+		self.output_folder = output_folder
 		self.tensor_data = {
-			"Time": [],
-			"00": [],
-			"01": [],
-			"02": [],
-			"10": [],
-			"11": [],
-			"12": [],
-			"20": [],
-			"21": [],
-			"22": []
+			"Time": [], "00": [], "01": [], "02": [], "10": [], "11": [], "12": [], "20": [], "21": [], "22": []
 		}
 
-	def record_values(self, tensor, t):
+	def save_results(self, time_list, eps_list):
+		for time, eps in zip(time_list, eps_list):
+			self.__record_values(eps, time)
+		self.__save()
+
+	def __record_values(self, tensor, t):
 		self.tensor_data["Time"].append(t)
 		for i in range(3):
 			for j in range(3):
 				self.tensor_data[f"{i}{j}"].append(tensor[i,j])
 
-	def save(self, output_folder):
-		if not os.path.exists(output_folder):
-			os.makedirs(output_folder)
+	def __save(self):
+		if not os.path.exists(self.output_folder):
+			os.makedirs(self.output_folder)
 		self.df = pd.DataFrame(self.tensor_data)
-		self.df.to_excel(os.path.join(output_folder, f"{self.name}.xlsx"))
+		self.df.to_excel(os.path.join(self.output_folder, f"{self.file_name}.xlsx"))
+
+class ViscoElastic():
+	def __init__(self, settings):
+		self.__load_time_list(settings)
+		self.__build_sigmas(settings)
+		self.__load_properties(settings)
+		self
+		self.__build_stress_increments()
+		self.__build_matrix()
+
+	def __load_time_list(self, settings):
+		self.time_list = settings["time"]
+
+	def __load_properties(self, settings):
+		self.E0 = settings["elasticity"]["E"]
+		self.nu0 = settings["elasticity"]["nu"]
+		self.voigt_E = settings["viscoelasticity"]["E"]
+		self.voigt_eta = settings["viscoelasticity"]["eta"]
+
+	def __build_sigmas(self, settings):
+		n = len(settings["sigma_xx"])
+		sigma_xx = np.array(settings["sigma_xx"]).reshape((1, n))
+		sigma_yy = np.array(settings["sigma_yy"]).reshape((1, n))
+		sigma_zz = np.array(settings["sigma_zz"]).reshape((1, n))
+		sigma_xy = np.array(settings["sigma_xy"]).reshape((1, n))
+		sigma_yz = np.array(settings["sigma_yz"]).reshape((1, n))
+		sigma_xz = np.array(settings["sigma_xz"]).reshape((1, n))
+		self.sigmas = np.concatenate((sigma_xx, sigma_yy, sigma_zz, sigma_xy, sigma_yz, sigma_xz)).T
+		self.sigma_0 = self.sigmas[0]
+
+	def __build_stress_increments(self):
+		self.d_sigmas = []
+		for i in range(1, len(self.sigmas)):
+			self.d_sigmas.append(self.sigmas[i] - self.sigmas[i-1])
+
+	def __build_matrix(self):
+		lame = self.E0*self.nu0/((1+self.nu0)*(1-2*self.nu0))
+		G = self.E0/(2 +2*self.nu0)
+		x = 1
+		self.C = np.array([
+					[2*G + lame, 	lame, 		lame, 		0.,		0., 	0.],
+					[lame, 			2*G + lame, lame, 		0.,		0., 	0.],
+					[lame, 			lame, 		2*G + lame, 0., 	0., 	0.],
+					[0., 			0., 		0., 		x*G,	0., 	0.],
+					[0., 			0., 		0., 		0., 	x*G, 	0.],
+					[0., 			0., 		0., 		0., 	0.,		x*G ],
+				])
+		self.D = np.linalg.inv(self.C)
+
+	def A(self, t):
+		value = 0.0
+		for E, eta in zip(self.voigt_E, self.voigt_eta):
+			r = E/eta
+			value += (1 - np.exp(-r*t))*self.E0/E
+		return 1 + value
+
+	def compute_strains(self):
+		self.eps_ve = []
+		for i in range(len(self.time_list)):
+			eps = self.A(self.time_list[i])*voigt2tensor(np.dot(self.D, self.sigma_0))
+			for j in range(i-1):
+				eps += self.A(self.time_list[i] - self.time_list[j])*voigt2tensor(np.dot(self.D, self.d_sigmas[j+1]))
+			self.eps_ve.append(eps)
+		self.eps_ve = np.array(self.eps_ve)
 
 class Creep():
 	def __init__(self, A, n):
@@ -94,47 +149,6 @@ class Creep():
 		# self.eps_cr = self.eps_cr_rate*t
 		self.t_old = t
 		self.eps_cr_old = self.eps_cr
-
-class ViscoElastic():
-	def __init__(self, E0, nu0, voigt_E, voigt_eta, sigma_0, d_sigmas, time_list, strain_0=voigt2tensor(build_stress(s_xx=0, s_yy=0, s_zz=0, s_xy=0, s_xz=0, s_yz=0))):
-		self.E0 = E0
-		self.nu0 = nu0
-		self.voigt_E = voigt_E
-		self.voigt_eta = voigt_eta
-		self.sigma_0 = sigma_0
-		self.time_list = time_list
-		self.d_sigmas = d_sigmas
-		self.strain_0 = strain_0
-		self.build_matrix()
-
-	def build_matrix(self):
-		lame = self.E0*self.nu0/((1+self.nu0)*(1-2*self.nu0))
-		G = self.E0/(2 +2*self.nu0)
-		x = 1
-		self.C = np.array([
-					[2*G + lame, 	lame, 		lame, 		0.,		0., 	0.],
-					[lame, 			2*G + lame, lame, 		0.,		0., 	0.],
-					[lame, 			lame, 		2*G + lame, 0., 	0., 	0.],
-					[0., 			0., 		0., 		x*G,	0., 	0.],
-					[0., 			0., 		0., 		0., 	x*G, 	0.],
-					[0., 			0., 		0., 		0., 	0.,		x*G ],
-				])
-		self.D = np.linalg.inv(self.C)
-
-	def A(self, t):
-		value = 0.0
-		for E, eta in zip(self.voigt_E, self.voigt_eta):
-			r = E/eta
-			value += (1 - np.exp(-r*t))*self.E0/E
-		return 1 + value
-
-	def compute_strains(self):
-		self.eps_ve = []
-		for i in range(len(self.time_list)):
-			eps = self.A(self.time_list[i])*voigt2tensor(np.dot(self.D, self.sigma_0))
-			for j in range(i-1):
-				eps += self.A(self.time_list[i] - self.time_list[j])*voigt2tensor(np.dot(self.D, self.d_sigmas[j+1]))
-			self.eps_ve.append(self.strain_0 + eps)
 
 class ViscoPlastic():
 	def __init__(self, A, n):
