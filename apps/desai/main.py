621fdbe055747986c737495321b3920369969404
Herminio <H.TasinafoHonorio@tudelft.nl>
2022-12-18 12:27:21 +0100

    Deleted unecessary subroutines.

--- apps/desai/main.py
+++ apps/desai/main.py
@@ -16,101 +16,6 @@ def save_json(data, file_name):
 	with open(file_name, "w") as f:
 	    json.dump(data, f, indent=4)
 
-def apply_white_theme(fig, axes, transparent=True):
-	fig.patch.set_facecolor("#212121ff")
-	if transparent:
-		fig.patch.set_alpha(0.0)
-	for ax in axes:
-		ax.grid(True, color='#c4c4c4ff')
-		ax.set_axisbelow(True)
-		ax.spines['bottom'].set_color('black')
-		ax.spines['top'].set_color('black')
-		ax.spines['right'].set_color('black')
-		ax.spines['left'].set_color('black')
-		ax.tick_params(axis='x', colors='black', which='both')
-		ax.tick_params(axis='y', colors='black', which='both')
-		ax.yaxis.label.set_color('black')
-		ax.xaxis.label.set_color('black')
-		ax.set_facecolor("#e9e9e9ff")
-
-def compute_I1_J2_Lode(s_xx, s_yy, s_zz, s_xy, s_xz, s_yz):
-	I1 = s_xx + s_yy + s_zz
-	I2 = s_xx*s_yy + s_yy*s_zz + s_xx*s_zz - s_xy**2 - s_yz**2 - s_xz**2
-	I3 = s_xx*s_yy*s_zz + 2*s_xy*s_yz*s_xz - s_zz*s_xy**2 - s_xx*s_yz**2 - s_yy*s_xz**2
-	J1 = 0
-	J2 = (1/3)*I1**2 - I2
-	J3 = (2/27)*I1**3 - (1/3)*I1*I2 + I3
-	lode_angle = []
-	for j2, j3 in zip(J2, J3):
-		value = sy.re((1/3)*sy.acos(-(j3*27**0.5)/(2*j2**1.5)))
-		lode_angle.append(np.degrees(float(value)))
-	return I1, np.sqrt(J2), np.array(lode_angle)
-
-def compute_invariants(settings):
-	s_xx = np.array(settings["sigma_xx"])/MPa
-	s_yy = np.array(settings["sigma_yy"])/MPa
-	s_zz = np.array(settings["sigma_zz"])/MPa
-	s_xy = np.array(settings["sigma_xy"])/MPa
-	s_yz = np.array(settings["sigma_yz"])/MPa
-	s_xz = np.array(settings["sigma_xz"])/MPa
-	I1, sqrt_J2, lode = compute_I1_J2_Lode(s_xx, s_yy, s_zz, s_xy, s_xz, s_yz)
-	return I1, sqrt_J2, lode
-
-def plot_stress_path(ax, I1, sqrt_J2):
-	ax.plot(I1, sqrt_J2, ".", color="forestgreen")
-	ax.set_xlabel(r"$I_1$ (MPa)", fontsize=12, fontname="serif")
-	ax.set_ylabel(r"$\sqrt{J_2}$ (MPa)", fontsize=12, fontname="serif")
-	ax.grid(True)
-
-def plot_yield_surface(ax, lode_angle, settings):
-	gamma = settings["viscoplastic"]["gamma"]
-	alpha = settings["viscoplastic"]["alpha_0"]
-	n = settings["viscoplastic"]["n"]
-	m = settings["viscoplastic"]["m"]
-	beta = settings["viscoplastic"]["beta"]
-	beta_1 = settings["viscoplastic"]["beta_1"]
-	sigma_t = settings["viscoplastic"]["sigma_t"]/MPa
-
-	# Yield surface
-	theta_rad = np.radians(lode_angle)
-	I1 = np.linspace(-sigma_t, 125, 500)
-	I1_star = I1 + sigma_t
-	J2 = (-alpha*I1_star**n + gamma*I1_star**2)*(np.exp(beta_1*I1_star) - beta*np.cos(3*theta_rad))**m
-	np.clip(J2, a_min=0.0, a_max=None, out=J2)
-
-	# Dilatancy boundary
-	J2_dil = (1 - 2/n)*gamma*(I1_star**2)*(np.exp(beta_1*I1_star) - beta*np.cos(3*theta_rad))**m
-
-	ax.plot(I1, np.sqrt(J2), "-", color="steelblue")
-	ax.plot(I1, np.sqrt(J2_dil), "--", color="lightcoral")
-	ax.set_ylim(0, 25)
-
-def plot_yield_surfaces(ax, lode_angle, settings):
-	gamma = settings["viscoplastic"]["gamma"]
-	alpha = settings["viscoplastic"]["alpha_0"]
-	n = settings["viscoplastic"]["n"]
-	m = settings["viscoplastic"]["m"]
-	beta = settings["viscoplastic"]["beta"]
-	beta_1 = settings["viscoplastic"]["beta_1"]
-	sigma_t = settings["viscoplastic"]["sigma_t"]/MPa
-
-	# Yield surface
-	theta_rad = np.radians(lode_angle)
-	I1 = np.linspace(-sigma_t, 125, 500)
-	I1_star = I1 + sigma_t
-	J2 = (-alpha*I1_star**n + gamma*I1_star**2)*(np.exp(beta_1*I1_star) - beta*np.cos(3*theta_rad))**m
-	np.clip(J2, a_min=0.0, a_max=None, out=J2)
-
-	# Dilatancy boundary
-	J2_dil = (1 - 2/n)*gamma*(I1_star**2)*(np.exp(beta_1*I1_star) - beta*np.cos(3*theta_rad))**m
-
-	ax.plot(I1, np.sqrt(J2), "-", color="steelblue")
-	ax.plot(I1, np.sqrt(J2_dil), "--", color="lightcoral")
-	ax.set_ylim(0, 25)
-
-
-
-
 def write_stresses():
 	j_file = "settings.json"
 
@@ -204,20 +109,6 @@ def main():
 
 	# Read settings
 	settings = read_json("settings.json")
-
-	# Compute invariants
-	I1, sqrt_J2, lode = compute_invariants(settings)
-
-	# Plot stress path and yield surface
-	fig, ax = plt.subplots(1, 1, figsize=(6, 3.6))
-	fig.subplots_adjust(top=0.92, bottom=0.135, left=0.105, right=0.985, hspace=0.2, wspace=0.2)
-
-	plot_stress_path(ax, I1, sqrt_J2)
-	plot_yield_surface(ax, 60, settings)
-
-	apply_white_theme(fig, [ax], transparent=True)
-	# plt.show()
-
 
 	# Initialize models
 	model_e = Elastic(settings)
